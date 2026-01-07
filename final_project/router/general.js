d6de85ea33ee71f13ff01a3e2442759c31a3ce22
Alandesson Linhares de Carvalho <alandessonlc@hotmail.com>
2026-01-07 13:03:11 -0500

    task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,9 +27,16 @@ public_users.get('/isbn/:isbn',function (req, res) {
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author?.toLowerCase();
+    const booksByAuthor = Object.values(books)
+                                .filter(book => book.author.toLowerCase() === author);
+
+    if (booksByAuthor.length > 0) {
+        res.json(booksByAuthor);
+    } else {
+        res.status(404).json({ message: "No books found for this author" });
+    }
 });
 
 // Get all books based on title
